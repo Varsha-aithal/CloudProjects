ffab35a28f7ca5250ec87a71e59089d64ea6b381
Varsha Aithal <varshaaithal30@gmail.com>
2026-02-03 16:53:57 +0100

    Add files to repo

--- Deploy-EC2-With-Terraform-GitHubActions/main.tf
+++ Deploy-EC2-With-Terraform-GitHubActions/main.tf
@@ -1,6 +1,28 @@
-provider "aws"
-{
+provider "aws"{
 region = "us-east-1"
 }
 
-resource 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.10.20260120.4-kernel-6.12-x86_64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] 
+}
+
+resource "aws_instance" "ec2_instance"{
+    ami = data.aws_ami.amazon_linux.id 
+    instance_type = "t2.micro"
+    key_name = "app-ssh-key"
+    tags = {
+        Name = var.ec2_name
+    }
+}
