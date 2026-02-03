bbd23892b9d22e7829a21ceaced8facc22450573
Varsha Aithal <varshaaithal30@gmail.com>
2026-02-03 17:25:10 +0100

    Switch AWS AMI from Amazon Linux to Ubuntu

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "aws"{
 region = "us-east-1"
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+data "aws_ami" "ubuntu" {
+    most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.10.20260120.4-kernel-6.12-x86_64-*"]
-  }
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+    }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"] 
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+    
+    owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "ec2_instance"{
-    ami = data.aws_ami.amazon_linux.id 
+    ami = data.aws_ami.ubuntu.id 
     instance_type = "t2.micro"
     key_name = "app-ssh-key"
     tags = {
