625f8d3767b60e874458e206cba3fe2dfd0c314a
Varsha Aithal <varshaaithal30@gmail.com>
2026-02-03 17:30:50 +0100

    Change EC2 instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "ec2_instance"{
     ami = data.aws_ami.ubuntu.id 
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = "app-ssh-key"
     tags = {
         Name = var.ec2_name
